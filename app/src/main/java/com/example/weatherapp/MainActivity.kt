969eb29db655d3ff0cf6137e7da5f7757b03cb54
Mingaleev-D92 <mingaleev.d92@mail.ru>
2022-06-10 13:12:23 +0300

    завершение верстки основного экрана

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.weatherapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.weatherapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            mainCityNameTv.text = "Moscow"
+            mainDataTv.text = "2 April"
+
+        }
     }
 }
